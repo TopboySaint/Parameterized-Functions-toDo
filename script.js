650a6672e202c432f041719788e5700b711358eb
TopboySaint <topboysaint1@gmail.com>
2024-03-23 11:52:43 +0100

    Santified a little

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function submitItems() {
     document.getElementById("errorMsg").style.display = "block";
   } else {
     items.push(floatingPassword.value);
-    console.log(items);
+    // console.log(items);
     showItems();
     document.getElementById("floatingPassword").value = "";
   }
@@ -16,9 +16,11 @@ function showItems() {
   showMe.innerHTML = "";
   items.map((products, i) => {
     showMe.innerHTML += `
-  <div class="card bg-primary" style="width: 18rem;">
+  <div class="card bg-primary" style="width:18rem;">
     <div class="card-body">
-      <h5 class="card-title text-center border-bottom pb-2 fs-3 text-white"><span>Item:</span> ${i + 1}</h5>
+      <h5 class="card-title text-center border-bottom pb-2 fs-3 text-white"><span>Item:</span> ${
+        i + 1
+      }</h5>
       <h6 class="card-subtitle text-center border-bottom pb-2 fs-4 text-white"><span>Product:</span> ${products}</h6>
       <button class="btn btn-sm my-3 mx-auto text-white btn-success w-50  me-5" onclick= "helpEdit(${i})" data-bs-toggle="modal" data-bs-target="#exampleModal">Edit</button>
       <button class="btn btn-sm my-3 mx-auto btn-danger " onclick="delETE(${i})">Delete</button>
@@ -36,16 +38,15 @@ function delETE(indexNum) {
   }
 }
 
-function helpEdit(){
-    inputIndex = i
+function helpEdit() {
+  inputIndex = i;
 }
 
-function editThis(){
-    if(newItem.value === ""){
-    document.getElementById('errorMsgIn').style.display = 'block'
-    } else{
-        items.splice( inputIndex, 1, newItem.value)
-        showItems()
-    }
+function editThis() {
+  if (newItem.value === "") {
+    document.getElementById("errorMsgIn").style.display = "block";
+  } else {
+    items.splice(inputIndex, 1, newItem.value);
+    showItems();
+  }
 }
-
