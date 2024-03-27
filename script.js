e81a6513d4743eb3bad81f6c193ecfab21289e1d
TopboySaint <topboysaint1@gmail.com>
2024-03-27 12:23:05 +0100

    fixing...

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 var items = [];
 var inputIndex;
 
-
 function submitItems() {
-  window.location.href = "show.html";
   var toDoList = {
     whatToDo: document.getElementById("floatingPassword").value,
     descOf: document.getElementById("floatingPasswordTwo").value,
@@ -13,61 +11,54 @@ function submitItems() {
   } else {
     console.log(toDoList);
     items.push(toDoList);
-    localStorage.setItem('items', JSON.stringify(items))
+    localStorage.setItem("itemsOn", JSON.stringify(items));
     // console.log(items);
     showItems();
     document.getElementById("floatingPassword").value = "";
     document.getElementById("floatingPasswordTwo").value = "";
+    window.location.href = "show.html";
   }
 }
 
-function showItems() {
-  // newItem.value = products;
-  if(getTheseItems){
-    getTheseItems.forEach(function(items) {
-        // console.log(items);
-        showMe.innerHTML = `
-        ${items}`
-    });
-}
-  showMe.innerHTML = "";
-  items.map((toDoList, i) => {
-    showMe.innerHTML += `
-  <div class="card bg-primary" style="width:18rem;">
-    <div class="card-body">
-      <h5 class="card-title text-center border-bottom pb-2 fs-3 text-white">${toDoList.whatToDo}</h5>
-      <p class="card-subtitle text-center border-bottom pb-2 text-white">${toDoList.descOf}</p>
-      <button class="btn btn-sm my-3 mx-auto text-white btn-warning w-25 h-25 me-5 ms-4" onclick= "helpEdit(${i})" data-bs-toggle="modal" data-bs-target="#exampleModal"><img src="Images/icons8-edit-24.png" alt="">
-      </button>
-      <button class="btn btn-sm my-3 mx-auto btn-danger w-25 h-25" onclick="delETE(${i})"><img src="Images/icons8-delete-30.png" alt="">
-      </button>
-    </div>
-  </div>
-        `;
-  });
-}
+// function showItems() {
+//   showMe.innerHTML = "";
+//   items.map((toDoList, i) => {
+//     showMe.innerHTML += `
+//   <div class="card bg-primary" style="width:18rem;">
+//     <div class="card-body">
+//       <h5 class="card-title text-center border-bottom pb-2 fs-3 text-white">${toDoList.whatToDo}</h5>
+//       <p class="card-subtitle text-center border-bottom pb-2 text-white">${toDoList.descOf}</p>
+//       <button class="btn btn-sm my-3 mx-auto text-white btn-warning w-25 h-25 me-5 ms-4" onclick= "helpEdit(${i})" data-bs-toggle="modal" data-bs-target="#exampleModal"><img src="Images/icons8-edit-24.png" alt="">
+//       </button>
+//       <button class="btn btn-sm my-3 mx-auto btn-danger w-25 h-25" onclick="delETE(${i})"><img src="Images/icons8-delete-30.png" alt="">
+//       </button>
+//     </div>
+//   </div>
+//         `;
+//   });
+// }
 
-function delETE(indexNum) {
-  var askQuest = confirm("Are you sure?");
-  if (askQuest === true) {
-    items.splice(indexNum, 1);
-    showItems();
-  }
-}
+// function delETE(indexNum) {
+//   var askQuest = confirm("Are you sure?");
+//   if (askQuest === true) {
+//     items.splice(indexNum, 1);
+//     showItems();
+//   }
+// }
 
-function helpEdit() {
-  inputIndex = i;
-}
+// function helpEdit() {
+//   inputIndex = i;
+// }
 
-function editThis() {
-  if (newItem.value === "") {
-    document.getElementById("errorMsgIn").style.display = "block";
-  } else {
-    var toDoList = {
-      whatToDo: document.getElementById("newItem").value,
-      descOf: document.getElementById("newItem2").value,
-    };
-    items.splice(inputIndex, 1, toDoList);
-    showItems();
-  }
-}
+// function editThis() {
+//   if (newItem.value === "") {
+//     document.getElementById("errorMsgIn").style.display = "block";
+//   } else {
+//     var toDoList = {
+//       whatToDo: document.getElementById("newItem").value,
+//       descOf: document.getElementById("newItem2").value,
+//     };
+//     items.splice(inputIndex, 1, toDoList);
+//     showItems();
+//   }
+// }
